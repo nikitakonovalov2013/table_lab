982483698ae99b2c5e0f73998389fb1fec579092
Konovalov.N <nikitakonovalov2013@yandex.ru>
2019-03-14 20:55:11 +0300

    2 - б) - завершено.

--- tab_f.c
+++ tab_f.c
@@ -6,7 +6,7 @@
 
 #define MAX_INT 9
 #define HEAD "Ключ №1\tКлюч №2\tЗначение\n"
-#define FORM "%i\t%i\t%s"
+#define FORM "%i\t%i\t%s\n"
 #define h(x) x%(tab->max_size)
 
 typedef struct {
@@ -30,14 +30,229 @@ static int tab_load(table *);
 static int save(table *);
 static int e_add(table *);
 static int e_find(table *, int , int);
-static int e_delete();
-static int e_print();
-static int t_print();
+static int e_delete(table *, int);
+static int e_print(table *, int);
+static int t_print(table *);
 static int get_keys(char **, char **, int);
 static int get_info(char **);
 static int get_int(int *);
 static int e_insert(table *, int, int, char *);
-static int dialog();
+static int dialog(table *);
+
+static int dialog(table *tab) {
+
+	char *c = (char *)malloc(10 * sizeof(char));
+	size_t b = 1;
+
+	printf("Выберите действие:\na - добавить элемент в таблицу;\np - вывести таблицу на экран;\nd - удалить элемент\nk - вывести элемент на экран\nm - показать меню\nq - выйти.\n");
+	do {
+		do {	
+			getline(&c,&b,stdin);
+			if (strlen(c) > 2 || strlen(c) < 2)
+				printf("Только один символ!\n");
+		} while(strlen(c) > 2 || strlen(c) < 2);
+
+		switch (c[0]) {
+			case 'a':
+				printf("ДОБАВЛЕНИЕ\n");
+				if(e_add(tab) == -1) {
+					printf("Ошибка при добавлении элемента.\n");
+				}
+				break;
+			case 'p':
+				t_print(tab);
+				break;
+			case 'd':
+				printf("УДАЛЕНИЕ\nВыберите пространство ключей (нажмите 1 или 2):\n");
+				char *s = (char *)malloc(10 * sizeof(char));
+				do {
+					getline(&s,&b,stdin);
+					if (strlen(s) > 2 || strlen(s) < 2)
+						printf("Только один символ!\n");
+				} while (strlen(s) > 2 || strlen(s) < 2);
+				
+				if (s[0] == '1') {
+					e_delete(tab, 1);
+				}
+				else if (s[0] == '2') {
+					e_delete(tab, 2);
+				} else {
+					printf("Неверное значение!\nМЕНЮ:\n");
+				}
+				free(s);
+				break;
+			case 'k':
+				printf("ПОИСК\nВыберите пространство ключей (нажмите 1 или 2):\n");
+				char *k = (char *)malloc(10 * sizeof(char));
+				do {
+					getline(&k,&b,stdin);
+					if (strlen(k) > 2 || strlen(k) < 2)
+						printf("Только один символ!\n");
+				} while (strlen(k) > 2 || strlen(k) < 2);
+				
+				if (k[0] == '1') {
+					e_print(tab, 1);
+				}
+				else if (k[0] == '2') {
+					e_print(tab, 2);
+				} else {
+					printf("Неверное значение!\nМЕНЮ:\n");
+				}
+				free(k);
+				break;
+					case 'm':
+				printf("Выберите действие:\na - добавить элемент в таблицу;\np - вывести таблицу на экран;\nd - удалить элемент\nk - вывести элемент на экран\nm - показать меню\nq - выйти.\n");
+				break;
+			default:
+				if (c[0] != 'q')
+					printf("Используйте команды из меню (m - вызвать меню)\n");
+				break;
+		}
+	} while (c[0] != 'q');
+	
+	free(c);
+	return 0;
+}
+static int e_delete(table *tab, int num) {
+
+	if (tab->t_size == 0) {
+		printf("Удаление невозможно. Таблица пуста!\n");
+		return -1;
+	}
+	
+	char *k1, *k2;
+	get_keys(&k1, &k2, num);
+	int i = 0;
+	if (num == 1) {
+		if ((i = e_find(tab, atoi(k1), -1)) >= 0) {
+			tab->check[i] = -1;
+			tab->t_size -= 1;
+			printf("Элемент успешно удален\n");
+			free(k1);
+			return 0;
+		} else {
+			printf("Элемент не найден!\n");
+			free(k1);
+			return -1;
+		}
+	}
+	if (num == 2) {
+		if ((i = e_find(tab, -1, atoi(k2))) >= 0) {
+			tab->check[i] = -1;
+			tab->t_size -= 1;
+			printf("Элемент успешно удален\n");
+			free(k2);
+			return 0;
+		} else {
+			printf("Элемент не найден!\n");
+			free(k2);
+			return -1;
+		}
+	}
+	return 0;
+}
+
+	
+static int t_print(table *tab) {
+
+	if (tab->t_size == 0) {
+		printf("Таблица пуста!\n");
+		return -1;
+	}
+
+	printf(HEAD);
+	char *info;
+	for (int i = 0; i < tab->max_size; i++) {
+			if (tab->check[i] == 1) {
+				
+				if ((info = (char *)malloc((tab->object[i].len) * sizeof(char))) == NULL) {
+					fprintf(stderr,"Невозможно выделить память!\n");
+					return -1;
+				}
+			
+				fseek(tab->fd, tab->object[i].offset, SEEK_SET);
+
+				if (fread(info, sizeof(char), tab->object[i].len, tab->fd) != tab->object[i].len) {
+					fprintf(stderr, "Не удалось считать данные.\n");
+					return -1;
+				}
+
+				printf(FORM, tab->object[i].key1, tab->object[i].key2, info);
+				free(info);
+			}
+	}
+	return 0;
+}
+	
+
+
+static int e_print(table *tab, int num) {
+
+	if (tab->t_size == 0) {
+		printf("Таблица пуста!\n");
+		return -1;
+	}
+	
+	int i = 0;
+	char *k1, *k2, *info;
+	get_keys(&k1, &k2, num);
+
+	if (num == 1) {
+		
+		if ((i = e_find(tab, atoi(k1), -1)) != -1) {
+			
+			if ((info = (char *)malloc((tab->object[i].len) * sizeof(char))) == NULL) {
+				fprintf(stderr,"Невозможно выделить память!\n");
+				return -1;
+			}
+			
+			fseek(tab->fd, tab->object[i].offset, SEEK_SET);
+
+			if (fread(info, sizeof(char), tab->object[i].len, tab->fd) != tab->object[i].len) {
+				fprintf(stderr, "Не удалось считать данные.\n");
+				return -1;
+			}
+
+			printf(HEAD);
+			printf(FORM, tab->object[i].key1, tab->object[i].key2, info);
+			free(info);
+			return 0;
+		} else {
+			printf("Элемент не найден!\n");
+			return 0;
+		}
+	}
+	
+	if (num == 2) {
+		
+		if ((i = e_find(tab, -1, atoi(k1))) != -1) {
+			
+			if ((info = (char *)malloc((tab->object[i].len) * sizeof(char))) == NULL) {
+				fprintf(stderr,"Невозможно выделить память!\n");
+				return -1;
+			}
+			
+			fseek(tab->fd, tab->object[i].offset, SEEK_SET);
+
+			if (fread(info, sizeof(char), tab->object[i].len, tab->fd) != tab->object[i].len) {
+				fprintf(stderr, "Не удалось считать данные.\n");
+				return -1;
+			}
+
+			printf(HEAD);
+			printf(FORM, tab->object[i].key1, tab->object[i].key2, info);
+			free(info);
+			return 0;
+		} else {
+			printf("Элемент не найден!\n");
+			return 0;
+		}
+	}
+			
+	return 0;
+}
+
+
 
 static int e_add(table *tab) {
 	
@@ -45,8 +260,10 @@ static int e_add(table *tab) {
 	get_keys(&k1, &k2, 0);
 	printf("Введите значение элемента: ");
 	get_info(&info);
+	int key1 = atoi(k1);
+	int key2 = atoi(k2);
 	
-	switch (e_insert(tab, atoi(k1), atoi(k2), info)) {
+	switch (e_insert(tab, key1, key2, info)) {
 		case 1:
 			printf("Данные ключи заняты!\n");
 			break;
@@ -179,7 +396,7 @@ static int get_int(int *a) {
 	if (strlen(line) < 0 || strlen(line) > MAX_INT)
 		return -1;
 	
-	for (int i = 0; i < strlen(line)-1; i++) {
+	for (int i = 0; i < strlen(line); i++) {
 		if ((line[0] == '0') || (isdigit(line[i]) == 0)) {
 			return -1;
 		}
@@ -258,7 +475,7 @@ static int open_f(table *tab, char *fname) {
 		return -1;
 	}
 	
-	if (fread(&tab->object, sizeof(item), tab->t_size, tab->fd) != tab->t_size) {
+	if (fread(tab->object, sizeof(item), tab->max_size, tab->fd) != tab->max_size) {
 		fprintf(stderr, "Не удалось считать данные из %s.\n", fname);
 		return -1;
 	}
@@ -328,7 +545,6 @@ static int tab_load(table *tab) {
 					flag = 0;
 					printf("Введите максимальный размер таблицы: ");
 					flag = get_int(&max_size);
-					printf("%i\n",flag);
 				} while (flag == -1);
 		
 				if (create_f(tab, fname, max_size) != 0) {
@@ -379,6 +595,12 @@ int main() {
 		fprintf(stderr, "Ошибка открытия/создания файла\n");
 		return -1;
 	}
-	fclose(tab.fd);
+
+	dialog(&tab);
+	save(&tab);
+	free(tab.check);
+	free(tab.object);
+	
+	return 0;
 }
 
